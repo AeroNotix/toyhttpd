3e019613fe9e24b83b14fd8142ccb5f1f3d43ad9
AeroNotix <aaron.l.france@gmail.com>
2013-02-05 12:24:10 +0100

    Whitespace.

--- fileio.c
+++ fileio.c
@@ -17,51 +17,51 @@ int numlength(off_t num) {
 }
 
 off_t fsize(const char *filename) {
-	struct stat st;
-	if (stat(filename, &st) == 0)
-		return st.st_size;
-	return -1;
+    struct stat st;
+    if (stat(filename, &st) == 0)
+        return st.st_size;
+    return -1;
 }
 
 char *readfile(char *filename) {
-	int c;
-	FILE *f;
-	off_t filesize;
-	char *buffer;
-	char *bufferp;
+    int c;
+    FILE *f;
+    off_t filesize;
+    char *buffer;
+    char *bufferp;
 
-	if ((filesize = fsize(filename)) == -1)
-		return NULL;
-	buffer = malloc(sizeof(char) * filesize);
-	bufferp = buffer;
-	if ((f = fopen(filename, "r")) == NULL) {
-		fprintf(stderr, "Error opening file: %s", filename);
-		return;
-	}
-	while ((c = getc(f)) != EOF)
-		*bufferp++ = c;
-	return buffer;
+    if ((filesize = fsize(filename)) == -1)
+        return NULL;
+    buffer = malloc(sizeof(char) * filesize);
+    bufferp = buffer;
+    if ((f = fopen(filename, "r")) == NULL) {
+        fprintf(stderr, "Error opening file: %s", filename);
+        return;
+    }
+    while ((c = getc(f)) != EOF)
+        *bufferp++ = c;
+    return buffer;
 }
 
 char **listdir(char *dir) {
-	DIR *dp;
-	int length, x;
-	char **dirs;
-	struct dirent *ep;
+    DIR *dp;
+    int length, x;
+    char **dirs;
+    struct dirent *ep;
 
-	dp = opendir(dir);
-	if (dp != NULL) {
-		dirs = malloc(sizeof(char*));
-		length = 0;
-		while (ep = readdir(dp)) {
-			dirs = realloc(dirs, sizeof(char*) * length + 1);
-			dirs[length++] = strdup(ep->d_name);
-		}
-		*(dirs + length) = NULL;
-		closedir(dp);
-		return dirs;
-	}
-	return NULL;
+    dp = opendir(dir);
+    if (dp != NULL) {
+        dirs = malloc(sizeof(char*));
+        length = 0;
+        while (ep = readdir(dp)) {
+            dirs = realloc(dirs, sizeof(char*) * length + 1);
+            dirs[length++] = strdup(ep->d_name);
+        }
+        *(dirs + length) = NULL;
+        closedir(dp);
+        return dirs;
+    }
+    return NULL;
 }
 
 int respond_with_file(int connfd, char* filename) {
